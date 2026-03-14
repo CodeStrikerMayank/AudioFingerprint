ed247bb260a1f062b72a7293c9bcf9f8da9f4606
CodeStrikerMayank <mayankbhatt9tha3@gmail.com>
2026-03-15 02:02:23 +0530

    New upgrade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import librosa
 import matplotlib.pyplot as plt
-
+import numpy as np 
 audio, sr = librosa.load("sample/allenginersrunning.wav")
 plt.figure(figsize=(13, 4))
 librosa.display.waveshow(audio, sr=sr)
@@ -9,4 +9,11 @@ plt.xlabel("Time", fontdict=font1)
 plt.ylabel("Amplitude", fontdict=font1)
 plt.title("Sound information ", fontdict=font1)
 plt.tight_layout()
+plt.show()
+fft = np.fft.fft(audio)
+fre = np.fft.fftfreq(len(audio))
+plt.plot(fft,np.abs(fft))
+plt.xlabel("frequency",fontdict=font1)
+plt.ylabel("magnitude",fontdict=font1)
+plt.title("Dull spectra details")
 plt.show()
