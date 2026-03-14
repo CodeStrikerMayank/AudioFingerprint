f087189f3d39a140df1e31c18564081fd878d573
CodeStrikerMayank <mayankbhatt9tha3@gmail.com>
2026-03-15 00:36:09 +0530

    major Start

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import librosa
-import matplotlib.pyplot as plt 
-audio ,sr = librosa.load("sample/allenginersrunning.wav")
-print(len(audio))
-print(sr)
+import matplotlib.pyplot as plt
+
+audio, sr = librosa.load("sample/allenginersrunning.wav")
+plt.figure(figsize=(13, 4))
+librosa.display.waveshow(audio, sr=sr)
+font1 = {"family": "serif", "color": "red", "size": 20}
+plt.xlabel("Time", fontdict=font1)
+plt.ylabel("Amplitude", fontdict=font1)
+plt.title("Sound information ", fontdict=font1)
+plt.tight_layout()
+plt.show()
