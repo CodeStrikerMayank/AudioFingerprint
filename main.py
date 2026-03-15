a0864209624a081dd9dbc41b800eaae60431c222
CodeStrikerMayank <mayankbhatt9tha3@gmail.com>
2026-03-16 00:50:40 +0530

    New upgrade

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import librosa
-import matplotlib.pyplot as plt
-import numpy as np 
-audio, sr = librosa.load("sample/allenginersrunning.wav")
-plt.figure(figsize=(13, 4))
-librosa.display.waveshow(audio, sr=sr)
-font1 = {"family": "serif", "color": "red", "size": 20}
-plt.xlabel("Time", fontdict=font1)
-plt.ylabel("Amplitude", fontdict=font1)
-plt.title("Sound information ", fontdict=font1)
-plt.tight_layout()
-plt.show()
-fft = np.fft.fft(audio)
-fre = np.fft.fftfreq(len(audio))
-plt.plot(fft,np.abs(fft))
-plt.xlabel("frequency",fontdict=font1)
-plt.ylabel("magnitude",fontdict=font1)
-plt.title("Dull spectra details")
-plt.show()
+import librosa as li
+import numpy as np
+import matplotlib.pyplot as pt
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def main(compare, target=None):
+    """ creating a logic to get percntage how much the values get match """
+    audio, sr = li.load(compare)
+    audio = li.util.fix_length(audio, size=5 * 20500)
+    data = li.feature.mfcc(y=audio, sr=sr, n_mfcc=13)
+    k = np.mean(data,axis=1)
+    audio1 , sr1 = li.load(target)
+    audio1 = li.util.fix_length(audio1,size=5*20500)
+    data1 = li.feature.mfcc(y=audio1,sr=sr1,n_mfcc=13)
+    k1 = np.mean(data1,axis=1)
+    value = cosine_similarity([k,k1])[0][0]
+    return value 
+
+print(main(compare="sample\enginersrunning.wav",target="sample\enginersrunning.wav"))
