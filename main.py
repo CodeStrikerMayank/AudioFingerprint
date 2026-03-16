d1b0dad68de97fda1efd37fedee6c24b73da1085
CodeStrikerMayank <mayankbhatt9tha3@gmail.com>
2026-03-17 00:34:40 +0530

    New logic

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
-import librosa as li
+import librosa as lb
 import numpy as np
-import conf
 import matplotlib.pyplot as pt
-from sklearn.metrics.pairwise import cosine_similarity
+from dtw import dtw
 
 
-def main(compare, target=None):
-    """creating a logic to get percntage how much the values get match"""
-    audio, sr = li.load(compare)
-    audio = li.util.fix_length(audio, size=5 * 20500)
-    data = li.feature.mfcc(y=audio, sr=sr, n_mfcc=13)
-    k = np.mean(data, axis=1)
-    audio1, sr1 = li.load(target)
-    audio1 = li.util.fix_length(audio1, size=5 * 20500)
-    data1 = li.feature.mfcc(y=audio1, sr=sr1, n_mfcc=13)
-    k1 = np.mean(data1, axis=1)
-    value = cosine_similarity([k, k1])[0][0]
-    return value
+def extract(file):
+    audio1, sr = lb.load(file)
+    audio = lb.util.normalize(audio1)
+    audio = lb.feature.mfcc(y=audio, sr=sr, n_mfcc=13)
+    data, magnitude = lb.piptrack(y=audio1, sr=sr)
+    pitch = []
+    for i in range(data.shape[1]):
+        index = magnitude[:, i].argmax()
+        pitch.append(data[index, i])
+
+    pitch = np.array(pitch)
+    pitch[pitch == 0] = np.nan
+    pitch = np.nanmean(pitch)
+    return audio, pitch
 
 
-# print(main(compare="sample\enginersrunning.wav",target="sample\enginersrunning.wav"))
-def waverform():
-    """creating wave graph to visualise the data"""
-    data, sr = li.load(conf.audio1)
-    pt.plot(data)
-    pt.show()
+def compare():
+    audio, pitch = extract(r"sample\Target hit.mp3")
+    audio1, pitch1 = extract(r"sample\Target hit.mp3")
+    result = dtw(audio.T, audio1.T)
+    distance = result.distance
+    pitche = abs(pitch - pitch1)
+    return distance, pitche
 
 
-def spectrum():
-    audio, sr = li.load(conf.audio1)
-    fft = np.fft.fft(audio)
-    freq = np.fft.fftfreq(len(audio))
-    pt.plot(fft, freq)
-    pt.show()
+def decision():
+    data, pitch = compare()
+    si = 100 / (1 + data)
+    print(round(si, 2), "%")
+    print(pitch, "hz")
 
-spectrum()
+
+decision()
